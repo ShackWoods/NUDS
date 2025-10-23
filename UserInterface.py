37f58178f24ce8760014598d053b116318624a90
Skyla Wakefield <ldyb96@durham.ac.uk>
2025-10-23 13:06:33 +0100

    Just a personal note for good practices

--- UserInterface.py
+++ UserInterface.py
@@ -45,7 +45,7 @@ class UI:
         for topic in self.all_topics:
             print(topic)
                 
-    #Can these filters be merged
+    #Can these filters be merged ***
     def add_topic_filter(self, filter: str):
         self.topic_filters.add(filter)
 
@@ -85,4 +85,23 @@ class UI:
 '''
 [1] - geeksforgeeks <https://www.geeksforgeeks.org/python/get-current-directory-python/> Accessed on 10/10/2025
 [2] - Nick on stackoverflow <https://stackoverflow.com/questions/434597/open-document-with-default-os-application-in-python-both-in-windows-and-mac-os> Accessed on 15/10/2025
+'''
+
+'''
+***NO
+The goals of modularisations:
+- Make it easy to read
+- Separate and solve subproblems
+- Make it easy to debug
+
+The last goal is actually two goals in one:
+- Make it easy to find and fix a problem
+- Make it easy to determine sufficient test cases for all that can go wrong*
+*For example, take the fun that is covering all reasonable states for a .docx file to be in when topic parsing
+*In that case we build the system to reduce how the user can interact
+
+This is also why switch-case statements are valid, as we can inherently design test cases according to the cases we match.
+
+Yes merging them makes the code smaller, but if I change the structure of the topic filter (for example)
+then the one function is no longer sufficient
 '''
